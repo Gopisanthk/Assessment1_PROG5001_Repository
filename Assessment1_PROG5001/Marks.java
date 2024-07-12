de75989a51b4c19bb96b7e746f6a27dfefb5a18d
Gopisanthk <162999414+Gopisanthk@users.noreply.github.com>
2024-07-12 23:37:23 +1000

    Get input marks and print marks

--- Assessment1_PROG5001/Marks.java
+++ Assessment1_PROG5001/Marks.java
@@ -1,33 +1,34 @@
 
 /**
- * Write a description of class Marks here.
+ * Assessment1 PROG5001
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Gopisanth Kobithasan)
+ * @version (20240712)
  */
+
+import java.util.Scanner;  
+
 public class Marks
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Marks
-     */
-    public Marks()
+    public static void main(String[] args)
     {
-        // initialise instance variables
-        x = 0;
+        Scanner sc=new Scanner(System.in);
+        int numberOfStudents = 30;
+        float[] marks = new float[numberOfStudents];
+        
+        System.out.println("Give marks of 30 students: ");
+        
+        for(int i=0; i<numberOfStudents; i++)
+        {
+            marks[i] = sc.nextFloat();
+        }  
+        
+        System.out.println("Marks of the students");
+        for(int j=0; j<numberOfStudents; j++)
+        {
+            System.out.println(marks[j]);
+        }
+        
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+ 
 }
